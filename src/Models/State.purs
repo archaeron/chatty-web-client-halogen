c89dde985c163f2519fc4bdff1ea1e30b257d03c
Nicolas Gagliani <whatamaps@gmail.com>
2015-05-01 16:33:39 +0200

    tabs to spaces :(

--- src/Models/State.purs
+++ src/Models/State.purs
@@ -7,19 +7,11 @@ import Models.Input
 import Models.Message
 import Models.User
 
--- type State =
--- 	{ messages :: [ Message ]
--- 	, editText :: String
--- 	, user :: User
--- 	, channels :: [ Channel ]
--- 	, selectedChannel :: Channel
--- 	, selectedInputType :: InputType
--- 	}
-
-data Input
-	= NewTask (Maybe String)
-	| UpdateDescription Number String
-	| MarkCompleted Number Boolean
-	| RemoveTask Number
-	| Undo
-	| Redo
+type State =
+    { messages :: [ Message ]
+    , editText :: String
+    , user :: User
+    , channels :: [ Channel ]
+    , selectedChannel :: Channel
+    , selectedInputType :: InputType
+    }
