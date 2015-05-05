ccd385a1c1faf128718cadb8f18697c1ef91b774
Nicolas Gagliani <whatamaps@gmail.com>
2015-05-05 10:42:57 +0200

    add an input element to the input view

--- src/Views/Input.purs
+++ src/Views/Input.purs
@@ -10,7 +10,18 @@ import qualified Halogen.HTML.Events as A
 import qualified Halogen.HTML.Events.Forms as A
 import qualified Halogen.HTML.Events.Handler as E
 
+import qualified Halogen.Themes.Bootstrap3 as B
+
+import Models.Action
+
 
 inputView :: forall m. (Alternative m) => H.HTML (m _)
 inputView =
-    H.div [ A.class_ $ A.className "input-view" ] [ H.text "input view" ]
+    H.div
+        [ A.class_ $ A.className "input-view" ]
+        [ H.input
+            [ A.classes [ B.formControl ]
+            , A.placeholder "Message"
+            ]
+            []
+        ]
