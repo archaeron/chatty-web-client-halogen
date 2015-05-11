b4521a78f7eaf88e30f09963c3ba56ab8b8f9cb2
Nicolas Gagliani <whatamaps@gmail.com>
2015-05-12 01:14:45 +0200

    not working ajax call

--- src/Views/Input.purs
+++ src/Views/Input.purs
@@ -4,17 +4,28 @@ module Views.Input
 
 import Control.Alternative
 
+import Data.Either
+import Data.Foreign.Class
+
+import Halogen
 import qualified Halogen.HTML as H
 import qualified Halogen.HTML.Attributes as A
 import qualified Halogen.HTML.Events as A
 import qualified Halogen.HTML.Events.Forms as A
 import qualified Halogen.HTML.Events.Handler as E
+import qualified Halogen.HTML.Events.Monad as E
 
 import qualified Halogen.Themes.Bootstrap3 as B
 
 import Models.Action
 import Models.Message
 
+import Control.Alt
+import Control.Monad.Eff
+import Control.Monad.Eff.Class
+import Control.Monad.Aff
+
+import Network.HTTP.Affjax
 
 inputView :: forall m. (Alternative m) => H.HTML (m _)
 inputView =
@@ -26,4 +37,20 @@ inputView =
 			, A.onValueChanged (A.input $ \text -> SendMessage $ TextMessage { text: text })
 			]
 			[]
+		, H.button
+			[ A.onClick  (\_ -> pure (handler "1+2"))
+			]
+			[]
 		]
+
+
+handler :: forall eff. String -> E.Event (HalogenEffects (ajax :: AJAX | eff)) Action
+handler code = E.yield (SendMessage $ TextMessage { text: "" }) `E.andThen` \_ -> E.async compileAff
+	where
+	compileAff :: Aff (HalogenEffects (ajax :: AJAX | eff)) Action
+	compileAff = do
+		result <- post "http://try.purescript.org/compile/text" code
+		let response = result.response
+		return case readProp "js" response <|> readProp "error" response of
+			Right js -> SendMessage $ TextMessage { text: js }
+			Left _ -> DoNothing
