42321d0a39e033e4093156c5c5870c965912bbd4
Nicolas Gagliani <whatamaps@gmail.com>
2015-05-15 23:43:58 +0200

    wip postfunction

--- src/Requests/Requests.purs
+++ src/Requests/Requests.purs
@@ -4,14 +4,28 @@ import Halogen (HalogenEffects())
 import qualified Halogen.HTML.Events.Handler as E
 import qualified Halogen.HTML.Events.Monad as E
 
+import Data.Maybe (Maybe(..), maybe)
 import Data.Either
 import Data.Foreign.Class
 import Control.Monad.Aff
 import Control.Alt
 import Network.HTTP.Affjax
+import Network.HTTP.Affjax.Request
+import Network.HTTP.Affjax.Response
+import Network.HTTP.RequestHeader (RequestHeader(..))
+import Network.HTTP.Method (Method(..), methodToString)
 
 import Models.Action
 import Models.Message
+
+postWithHeaders :: forall e a b. (Requestable a, Respondable b) => [RequestHeader] -> URL -> a -> Affjax e b
+postWithHeaders headers url content =
+	affjax $ defaultRequest
+		{ method = POST
+		, url = url
+		, content = Just content
+		, headers = headers
+		}
 
 handler :: forall eff. String -> E.Event (HalogenEffects (ajax :: AJAX | eff)) Action
 handler code = E.yield DoNothing `E.andThen` \_ -> E.async compileAff
